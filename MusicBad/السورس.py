ce82b002f94cd06e1f953a328be84ae4a4713476
Badsource <106994803+Badsource@users.noreply.github.com>
2022-06-10 01:05:28 +0300

    Delete MusicBad directory

--- MusicBad/السورس.py
+++
@@ -1,105 +0,0 @@
-import os
-import sys
-from datetime import datetime
-from time import time
-
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-from config import HNDLR, SUDO_USERS, OWNER_NAME, CHANNEL
-
-START_TIME = datetime.utcnow()
-TIME_DURATION_UNITS = (
-    ("الأحد", 60 * 60 * 24 * 7),
-    ("يوم", 60 * 60 * 24),
-    ("الساعة", 60 * 60),
-    ("الدقيقة", 60),
-    ("الثانيه", 1),
-)
-
-
-async def _human_time_duration(seconds):
-    if seconds == 0:
-        return "inf"
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else ""))
-    return ", ".join(parts)
-
-
-@Client.on_message(filters.command(["بنك"], prefixes=f"{HNDLR}"))
-async def ping(client, m: Message):
-    await m.delete()
-    start = time()
-    current_time = datetime.utcnow()
-    m_reply = await m.reply_text("⚡")
-    delta_ping = time() - start
-    uptime_sec = (current_time - START_TIME).total_seconds()
-    uptime = await _human_time_duration(int(uptime_sec))
-    await m_reply.edit(
-        f"<b>🏓 بـنـك/b> `{delta_ping * 1000:.3f} بالثانيه` \n<b>⏳ شغال</b> - `{uptime}`"
-    )
-
-
-@Client.on_message(
-    filters.user(SUDO_USERS) & filters.command(["اعادة تشغيل"], prefixes=f"{HNDLR}")
-)
-async def restart(client, m: Message):
-    await m.delete()
-    jepthon = await m.reply("1")
-    await jepthon.edit("2")
-    await jepthon.edit("3")
-    await jepthon.edit("4")
-    await jepthon.edit("5")
-    await jepthon.edit("6")
-    await jepthon.edit("7")
-    await jepthon.edit("8")
-    await jepthon.edit("9")
-    await jepthon.edit("**تم اعادة تشغيل سورس باد ميوزك بنجاح ✓**")
-    os.execl(sys.executable, sys.executable, *sys.argv)
-    quit()
-
-
-@Client.on_message(filters.command(["الاوامر","اوامري"], prefixes=f"{HNDLR}"))
-async def help(client, m: Message):
-    await m.delete()
-    JEPM = f"""
-👋 اهلا {m.from_user.mention}!
-قناة السورس [ {OWNER_NAME} ](t.me/{CHANNEL})
-══════⊹⊱≼≽⊰⊹══════
-☢ | لتشغيل صوتية في المكالمة أرسل ↢ 『 `{HNDLR}شغل  + اسم الاغنية` 』
-☢ | لتشغيل فيديو في المكالمة  ↢ 『 `{HNDLR}شغل فيديو  + اسم الاغنية` 』
-══════⊹⊱≼≽⊰⊹══════
-☢ | لأيقاف الاغنية او الفيديو مؤقتآ  ↢ 『 `{HNDLR}كمل` 』 
-☢ | لأيقاف الاغنية  ↢ 『 `{HNDLR}اوكف` 』 
-☢ | لتخطي الاغنية الحالية و تشغيل الاغنية التالية ↢ 『 `{HNDLR}سكب` 』
-☢ | لتشغيل الاغنية عشوائية من قناة او مجموعة  ↢ 『 `{HNDLR}اغنيه عشوائية` 』
-══════⊹⊱≼≽⊰⊹══════
-☢ | لتحميل صوتية أرسل ↢ 『 `{HNDLR}تحميل + اسم الاغنية او الرابط` 』
-☢ | لتحميل فيديو  ↢  『 `{HNDLR}تحميل_فيديو + اسم الاغنية او الرابط` 』
-══════⊹⊱≼≽⊰⊹══════
-☢ | لأعاده تشغيل التنصيب أرسل ↢  『 `{HNDLR}ريستارت` 』
-══════⊹⊱≼≽⊰⊹══════
-المطور  ⁝ ❪{OWNER_NAME}❫
-القناة  ⁝ ❪@{CHANNEL}❫
-"""
-    await m.reply(JEPM)
-
-
-@Client.on_message(filters.command(["السورس"], prefixes=f"{HNDLR}"))
-async def repo(client, m: Message):
-    await m.delete()
-    bad = f"""
-<b>- مرحبا {m.from_user.mention}!
-
-🎶 هذا هو سورس باد ميوزك
-
-🤖  اختصاص هذا البوت لتشغيل مقاطع صوتية او مقاطع الفيديو في المكالمات الصوتية
-
-⚒️ لعرض اوامر السورس ارسل  {HNDLR}اوامري
-
-📚 • قناة سورس باد  : @Colli9</b>
-"""
-    await m.reply(bad, disable_web_page_preview=True)
