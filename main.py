2ad9c89bb7eb374b22458cd2b2f372c99bd953f2
Badsource <106994803+Badsource@users.noreply.github.com>
2022-06-10 10:01:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -367,5 +367,5 @@ def redirect_message():
 
 if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url="https://anestsh.herokuapp.com/"+str(BOT_TOKEN))
+    bot.set_webhook(url="https://dragon16.herokuapp.com/"+str(BOT_TOKEN))
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
